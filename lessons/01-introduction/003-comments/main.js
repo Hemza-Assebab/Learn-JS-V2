d5bce4bceb3c9576436fa067ba2f2d7a5391a2ec
Hemza-Assebab <assebabhamza03@gmail.com>
2025-07-03 17:57:00 +0100

    Comments

---
+++ lessons/01-introduction/003-comments/main.js
@@ -0,0 +1,26 @@
+/* Comments */
+/*
+    -Comments in code are notes written by developers to explain what the code does
+    -They are ignored by the computer when the program runs, so they don't affect how the program works
+*/
+
+/* Why use comments ? */
+/*
+    -To explain what a piece of code is doing
+    -To remind yourself (or others) why something was done a certain way
+    -To temporarily disable parts of code for testing
+    -To document the code
+*/
+
+/* Types of comments in JS */
+// This is a single line comment
+/*
+    This
+    is a
+    multiple
+    line
+    comment
+*/
+
+/* Examle */
+console.log("Hello"); // This shows a message in the console
