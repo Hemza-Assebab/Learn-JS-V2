b9897794386e99aa7de3620e3d7e7a86372c2b63
Hemza-Assebab <assebabhamza03@gmail.com>
2025-07-03 22:22:44 +0100

    Web APIs Explained

---
+++ lessons/01-introduction/005-web-apis-explained/main.js
@@ -0,0 +1,29 @@
+/* Web APIs Explained */
+/*
+    -A Web API (Web Application Programming Interface) is a set of built-in features provided by the browser
+     (not JavaScript itself) that allows JavaScript to interact with the web browser, system, or the internet
+
+    -JavaScript alone is a small language, Web APIs make it powerful by giving it access to things like
+     the web page, user input, the internet, and more
+
+    -In Short: Web APIs are tools given by the browser to help JavaScript interact with the outside world
+*/
+
+/* What do Web APIs do ? */
+/*
+    -They let JavaScript:
+
+    # Interact with the DOM (Document Object Model)
+    # Handle events
+    # Work with HTTP requests
+    # Use timing functions
+    # Access local storage
+    # Work with geolocation, notifications, canvas, and more
+*/
+
+/* How does it relate to JavaScript ? */
+// JavaScript calls these Web APIs when you write things like:
+window.alert("Hello !");
+fetch("http://localhost:3000");
+document.body;
+// The browser runs the API code outside JavaScript and gives the results back
