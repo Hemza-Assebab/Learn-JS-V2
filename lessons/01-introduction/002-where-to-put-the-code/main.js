6887bfa42a612463ce764920292fa56935fdf08e
Hemza-Assebab <assebabhamza03@gmail.com>
2025-07-03 17:44:55 +0100

    Where To Put JS Code

---
+++ lessons/01-introduction/002-where-to-put-the-code/main.js
@@ -0,0 +1,24 @@
+/* Where To Put JS Code */
+/*
+    There are two ways to write JS code for your project
+*/
+
+/* Script Tag */
+/*
+    -You can write JS code on HTML files by creating "script" tag and start writing JS code inside of them
+    -Check the HTML file existing in this folder
+    -Example:
+        <script>
+            console.log("Hello from HTML file");
+        </script>
+*/
+
+/* JS file */
+/*
+    -You can create a JS file and link it with the HTML file you are working on
+    -Example:
+        <script src="main.js" ></script>
+        // Note: You can link many JS files to an HTML file
+        <script src="main2.js" ></script>
+*/
+console.log("Hello from JS file");
