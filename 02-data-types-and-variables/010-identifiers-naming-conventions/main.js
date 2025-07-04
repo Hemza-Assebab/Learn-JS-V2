e649ba76e6bf273df05d1452444ae37928d2dd3e
Hemza-Assebab <assebabhamza03@gmail.com>
2025-07-04 20:55:18 +0100

    Identifiers Naming Conventions

---
+++ 02-data-types-and-variables/010-identifiers-naming-conventions/main.js
@@ -0,0 +1,14 @@
+/* Identifiers Naming Conventions */
+// Identifiers are variables, functions,  classes names
+/*
+    - [1]  Identifiers can't contain space
+    - [2]  Cannot start with a number
+    - [3]  Can start with (_, $)
+    - [4]  Can contain (letters, digits, underscore, $)
+    - [5]  Cannot contain special characters (#, -, @, %...)
+    - [6]  Must not be a reserved keyword in JavaScript (var, function, for, let, if...)
+    - [7]  Identifiers are case-sensitive
+    - [8]  Use camelCase for variables and functions (userName)
+    - [9]  Use PascalCase for class names or constructors (UserProfile)
+    - [10] Use ALL_CAPS with underscores for constants (MAX_USERS)
+*/
