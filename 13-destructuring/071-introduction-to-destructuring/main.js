a50b78dbac9b02fe35e1368331e7a90de23a2b2c
Hemza-Assebab <assebabhamza03@gmail.com>
2025-08-05 15:50:31 +0100

    Introduction To Destructuring

---
+++ 13-destructuring/071-introduction-to-destructuring/main.js
@@ -0,0 +1,21 @@
+/* Introduction To Destructuring */
+/*
+    -Destructuring is a feature in JavaScript that lets you extract values from arrays or properties
+     from objects and assign them to variables in a single, concise line of code
+    -It makes your code cleaner, easier to read, and helps avoid repetitive property access like (obj.prop1, obj.prop2...)
+*/
+
+/* Types Of Destructuring */
+/*
+    -Array Destructuring  => Extract values based on position
+    -Object Destructuring => Extract values based on property names
+*/
+
+/* Why Use Destructuring ? */
+/*
+    -Cleaner code
+    -Avoid repeating `obj.` or `arr[i]`
+    -Easier function parameter access
+    -Sets default values easily
+    -Works well with function returns
+*/
